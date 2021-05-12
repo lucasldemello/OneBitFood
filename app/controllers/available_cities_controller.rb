b3f98511b1841d8a9d6e9462a15064d3e59f94a4
Lucas de Melo <lucas.melo@mpgo.mp.br>
2021-05-11 21:47:55 -0300

    Aula 1 - OneBitFood

---
+++ app/controllers/available_cities_controller.rb
@@ -0,0 +1,6 @@
+class AvailableCitiesController < ApplicationController
+  def index
+    @available_cities = Restaurant.all.map { |r| r.city }.uniq
+    render json: @available_cities
+  end
+end
---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,35 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: :show
+  
+  def index
+    @restaurants = Restaurant.all
+    filter_by_query if params[:q].present?
+    filter_by_city if params[:city].present?
+    filter_by_category if params[:category].present?
+    render json: @restaurants
+  end
+
+  def show
+
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+    render json: @restaurant
+  end
+
+  def filter_by_query
+    @restaurants = @restaurants.ransack(name_or_description_cont: params[:q]).result
+  end
+
+  def filter_by_city
+    @restaurants = @restaurants.where(city: params[:city])
+  end
+
+  def filter_by_category
+    @restaurants = @restaurants.select do |r|
+      r.category.title == params[:category]
+    end
+  end
+
+end
